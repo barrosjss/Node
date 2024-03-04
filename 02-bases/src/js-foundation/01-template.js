d0e30c5588c8ab9aa12151e509eeaac4cc10de16
tract0rb <jesus.reinaldo.barros@gmail.com>
2024-03-04 02:09:06 -0500

    add::01-template.js

---
+++ 02-bases/src/js-foundation/01-template.js
@@ -0,0 +1,11 @@
+const emailTemplate = `
+    <div>
+        <h1>Hi,{{name}}</h1>
+        <p>Thanks you for your order.</p>
+    </div>
+`;
+
+// console.log(emailTemplate);
+module.exports = {
+  emailTemplate,
+};
